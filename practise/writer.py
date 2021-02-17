ceefa37319859c0e613b9ea128e96a96187185bf
thomplth <29663345+thomplth@users.noreply.github.com>
2021-02-18 05:05:53 +0800

    Fixed format

--- practise/writer.py
+++ practise/writer.py
@@ -1,5 +1,8 @@
 def write(solution, filename):
+    filename = 'output/' + filename
     with open(filename, 'w') as fo:
         fo.write(str(len(solution))+'\n')
         for elt in solution:
-            fo.write(str(elt)+' ')
+            fo.write(str(len(elt)) + ' ' + str(elt).strip('[]').replace(',', ''))
+            fo.write('\n')
+        fo.close
