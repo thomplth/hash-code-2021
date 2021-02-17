871c54f0d00e6770f85b9027e6e0a3f07ad13702
thomplth <29663345+thomplth@users.noreply.github.com>
2021-02-18 05:03:45 +0800

    Minor fix, issues remain in define_team

--- practise/optimize.py
+++ practise/optimize.py
@@ -1,6 +1,7 @@
 # Let numOfPizza = n, numOfIngredient = d
 import sys
 
+
 def solution(pizzaData):
     twoSolution = []
     threeSolution = []
@@ -29,21 +30,21 @@ def solution(pizzaData):
                     break
         if not pizzaPicked:
             nowMaxIndex -= 1
-            
+
     # Distribute the another pizza to two-person team
     # Sort the entire current two person team by pizza length
 
-    # Assign the remaining pizza to the two-people teams according to which 
+    # Assign the remaining pizza to the two-people teams according to which
 
 
 def countDistinctIngredients(pizzas):
-     ingredients = set()
-     for pizza in pizzas:
-         for ingredient in pizza[1:]:
-             ingredients.add(ingredient)
-     return len(ingredients)
+    ingredients = set()
+    for pizza in pizzas:
+        for ingredient in pizza[1:]:
+            ingredients.add(ingredient)
+    return len(ingredients)
 
-        
+
 """
 def sort_ingredient_list(pizzaData):
     minIngredientList = []
@@ -54,34 +55,39 @@ def sort_ingredient_list(pizzaData):
         elif count == minIngredientCount:
             minIngredientList.append(ingredient)
     return [minIngredientList, minIngredientCount]
-"""            
+"""
 
 
 def define_team(pizzaData):
     # First, maximize number two person team
-    twoPersonTeam = pizzaData.nOfTwo
-    fourPersonTeam = 3 - ((pizzaData.nOfPizzas - 2 * pizzaData.nOfTwo) % 3)
-    threePersonTeam = pizzaData.nOfPizzas - twoPersonTeam - fourPersonTeam
-    return [twoPersonTeam,threePersonTeam,fourPersonTeam]
+    twoPersonTeam = pizzaData['nOfTwo']
+    fourPersonTeam = 3 - ((pizzaData['nOfPizzas'] - 2 * pizzaData['nOfTwo']) % 3)
+    threePersonTeam = pizzaData['nOfPizzas'] - twoPersonTeam - fourPersonTeam
+    # print(pizzaData['nOfTwo'], pizzaData['nOfThree'], pizzaData['nOfFour'])
+    # print([twoPersonTeam, threePersonTeam, fourPersonTeam])
+    return [twoPersonTeam, threePersonTeam, fourPersonTeam]
+
 
 def hash_apporach_for_two(dataset):
-    arr = [[0 for i in range(dataset['nOfPizzas'])] for j in range(dataset['nOfPizzas'])]
+    arr = [[0 for i in range(dataset['nOfPizzas'])]
+           for j in range(dataset['nOfPizzas'])]
     for i in range(dataset['nOfPizzas']):
-        i_total_ingra=1
-        i_ingra=set()
-        while i_total_ingra<(len(dataset['pizzas'][i])):
+        i_total_ingra = 1
+        i_ingra = set()
+        while i_total_ingra < (len(dataset['pizzas'][i])):
             i_ingra.add(dataset['pizzas'][i][i_total_ingra])
-            i_total_ingra+=1
-        
+            i_total_ingra += 1
+
         for j in range(dataset['nOfPizzas']):
-            if i==j:
-                arr[dataset['pizzas'][i][0]][dataset['pizzas'][j][0]]=-1
+            if i == j:
+                arr[dataset['pizzas'][i][0]][dataset['pizzas'][j][0]] = -1
             else:
-                j_total_ingra=1
-                j_ingra=set()
-                while j_total_ingra<(len(dataset['pizzas'][i])):
+                j_total_ingra = 1
+                j_ingra = set()
+                while j_total_ingra < (len(dataset['pizzas'][i])):
                     j_ingra.add(dataset['pizzas'][j][j_total_ingra])
-                    j_total_ingra+=1
-                j_ingra=j_ingra.union(i_total_ingra)
-                arr[dataset['pizzas'][i][0]][dataset['pizzas'][j][0]]=len(j_ingra)
+                    j_total_ingra += 1
+                j_ingra = j_ingra.union(i_total_ingra)
+                arr[dataset['pizzas'][i][0]
+                    ][dataset['pizzas'][j][0]] = len(j_ingra)
     print(arr)
